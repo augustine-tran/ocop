e0d5e305dca5647decf7dd577e5ae5c14b0c2a06
Khanh Tran <donga.spirit@gmail.com>
2023-12-21 14:38:22 +0700

    add transition effect for modal

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -1,21 +1,85 @@
 import { Controller } from '@hotwired/stimulus';
+import { enter, leave } from './transition';
 
 export default class extends Controller {
-  static targets = ['container', 'background', 'trigger'];
+  static targets = ['container', 'background', 'panel', 'trigger'];
+  static values = {
+    open: { type: Boolean, default: false },
+    restoreScroll: { type: Boolean, default: true },
+  };
+
+  disconnect() {
+    this.close();
+  }
 
   open() {
-    this.triggerTarget.classList.add('hidden');
-    this.backgroundTarget.classList.remove('hidden');
-    this.backgroundTarget.classList.add('opacity-100', 'translate-y-0', 'scale-100');
-    this.containerTarget.classList.remove('hidden');
-    this.containerTarget.classList.add('opacity-100', 'translate-y-0', 'scale-100');
+    this.openValue = true;
   }
 
   close() {
-    this.triggerTarget.classList.remove('hidden');
-    this.backgroundTarget.classList.remove('opacity-100', 'translate-y-0', 'scale-100');
-    this.backgroundTarget.classList.add('hidden');
-    this.containerTarget.classList.remove('opacity-100', 'translate-y-0', 'scale-100');
-    this.containerTarget.classList.add('hidden');
+    this.openValue = false;
+  }
+
+  closeBackground(event) {
+    if (event.target === this.backgroundTarget) this.close();
+  }
+
+  async openValueChanged() {
+    if (this.openValue) {
+      this.containerTarget.focus();
+      this.lockScroll();
+      enter(this.backgroundTarget);
+      enter(this.containerTarget);
+      enter(this.panelTarget);
+      leave(this.triggerTarget);
+    } else {
+      leave(this.containerTarget);
+      leave(this.panelTarget);
+      enter(this.triggerTarget);
+      await leave(this.backgroundTarget);
+      this.unlockScroll();
+    }
+  }
+
+  lockScroll() {
+    // Save the scroll position before we hide the scrollbar
+    if (this.restoreScrollValue) {
+      this.saveScrollPosition();
+      // Add negative top position in order for body to stay in place
+      document.body.style.top = `-${this.scrollPosition}px`;
+    }
+
+    // Add right padding to the body so the page doesn't shift
+    // when we disable scrolling
+    const scrollbarWidth = window.innerWidth - document.documentElement.clientWidth;
+    document.body.style.paddingRight = `${scrollbarWidth}px`;
+
+    // Add classes to body to fix its position
+    document.body.classList.add('fixed', 'inset-x-0', 'overflow-hidden');
+  }
+
+  unlockScroll() {
+    // Remove tweaks for scrollbar
+    document.body.style.paddingRight = null;
+
+    // Remove classes from body to unfix position
+    document.body.classList.remove('fixed', 'inset-x-0', 'overflow-hidden');
+
+    // Restore the scroll position of the body before it got locked
+    if (this.restoreScrollValue) {
+      this.restoreScrollPosition();
+
+      // Remove the negative top inline style from body
+      document.body.style.top = null;
+    }
+  }
+
+  saveScrollPosition() {
+    this.scrollPosition = window.pageYOffset || document.body.scrollTop;
+  }
+
+  restoreScrollPosition() {
+    if (this.scrollPosition === undefined) return;
+    document.documentElement.scrollTop = this.scrollPosition;
   }
 }
