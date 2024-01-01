5f3794aa415e7022fe8221876f9b3a2f3dac2695
Khanh Tran <donga.spirit@gmail.com>
2024-01-01 10:30:47 +0700

    direct upload with dropzone

--- config/importmap.rb
+++ config/importmap.rb
@@ -10,3 +10,8 @@ pin_all_from 'app/javascript/controllers', under: 'controllers'
 pin 'trix'
 pin '@rails/actiontext', to: 'actiontext.js'
 pin '@hotwired/turbo-rails', to: 'turbo.min.js', preload: true
+
+# Use direct uploads for Active Storage (remember to import "@rails/activestorage" in your application.js)
+pin '@rails/activestorage', to: 'activestorage.esm.js'
+pin 'dropzone', to: 'https://ga.jspm.io/npm:dropzone@6.0.0-beta.2/dist/dropzone.mjs'
+pin 'just-extend', to: 'https://ga.jspm.io/npm:just-extend@5.1.1/index.esm.js'
