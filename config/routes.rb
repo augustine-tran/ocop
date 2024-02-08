62cfe32b8acacd418dcdeff30db6cb56ac3a97c9
Khanh Tran <donga.spirit@gmail.com>
2024-02-08 13:46:16 +0700

    setup kamal

--- config/routes.rb
+++ config/routes.rb
@@ -33,7 +33,7 @@ Rails.application.routes.draw do
   resources :scores do
     resources :evidences, controller: 'scores/evidences' do
       member do
-        patch :move_image, to: 'scores/evidences/move_image'
+        patch :move_image, to: 'scores/evidences#move_image'
       end
     end
   end
