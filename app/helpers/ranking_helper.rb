6addd8be2ac897c829ec3ebb5582e9bd70a366c3
Khanh Tran <donga.spirit@gmail.com>
2024-03-13 19:42:47 +0700

    fix bug if rank is not a numeric

--- app/helpers/ranking_helper.rb
+++ app/helpers/ranking_helper.rb
@@ -27,7 +27,9 @@ module RankingHelper
     end.join.html_safe # rubocop:disable Rails/OutputSafety
   end
 
-  def render_ranks(rank)
+  def render_ranks(rank = 0)
+    raise ArgumentError, 'Rank must be a number' unless rank.is_a? Numeric
+
     (1..rank).map do
       '⭐️'
     end.join
